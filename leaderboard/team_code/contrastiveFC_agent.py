36acf62c564def4633d4e532e68c981c01c3fd82
yewon-lee <yewonlee.lee@mail.utoronto.ca>
2021-07-28 16:59:03 -0400

    fixed major bugs

--- leaderboard/team_code/contrastiveFC_agent.py
+++ leaderboard/team_code/contrastiveFC_agent.py
@@ -13,7 +13,7 @@ import numpy as np
 from PIL import Image
 
 from leaderboard.autoagents import autonomous_agent
-from contrastiveFC.models import ContrastiveLearningModel, ControlsModelFC
+from contrastiveFC.models import ContrastiveLearningModel,  ControlsModel_FC
 from contrastiveFC.config import GlobalConfig
 from contrastiveFC.data import scale_and_crop_image, transform_2d_points, lidar_to_histogram_features
 from team_code.planner import RoutePlanner
@@ -31,14 +31,14 @@ def get_entry_point():
 class ContrastiveFC_Agent(autonomous_agent.AutonomousAgent):
 	def setup(self, path_to_conf_file):
 		# Import autopilot class (for throttle/brake later on)
-        AutoPilot = AutoPilot(autonomous_agent.AutonomousAgent)
+		#AutoPilot = AutoPilot(autonomous_agent.AutonomousAgent)
 		self.track = autonomous_agent.Track.SENSORS
 		self.config_path = path_to_conf_file
 		self.step = -1
 		self.wall_start = time.time()
 		self.initialized = False
 
-		self.input_buffer = {'rgb': deque(), 'lidar': deque()}
+		self.input_buffer = {'rgb': deque(), 'rgb_left': deque(), 'rgb_right': deque, 'rgb_rear': deque(), 'lidar': deque(), 'gps': deque(), 'thetas': deque()}
 
 		# TODO: path to trained models
 		cvm_path = '/home/yewon/semi-hard-negative-mining/models/13-Hard-NegSampling-32Batch/bestContrastive.pt'
@@ -49,11 +49,11 @@ class ContrastiveFC_Agent(autonomous_agent.AutonomousAgent):
 
 		# Load trained model
 		self.net_contrastive = ContrastiveLearningModel().cuda()
-		self.net_conrastive.load_state_dict(torch.load(cvm_path))
+		self.net_contrastive.load_state_dict(torch.load(cvm_path))
 		self.net_contrastive.eval()
-		self.net_controls = ControlsModel_FC().cuda()
+		self.net_controls = ControlsModel_FC(self.net_contrastive).cuda()
 		self.net_controls.load_state_dict(torch.load(clm_path))
-		self.net_conrols.eval()
+		self.net_controls.eval()
 
 		self.save_path = None
 		if SAVE_PATH is not None:
@@ -110,12 +110,12 @@ class ContrastiveFC_Agent(autonomous_agent.AutonomousAgent):
 					'width': 400, 'height': 300, 'fov': 100,
 					'id': 'rgb_rear'
 					},
-                {   
-                    'type': 'sensor.lidar.ray_cast',
-                    'x': 1.3, 'y': 0.0, 'z': 2.5,
-                    'roll': 0.0, 'pitch': 0.0, 'yaw': -90.0,
-                    'id': 'lidar'
-                    },
+				{	
+				'type': 'sensor.lidar.ray_cast',
+				'x': 1.3, 'y': 0.0, 'z': 2.5,
+				'roll': 0.0, 'pitch': 0.0, 'yaw': -90.0,
+				'id': 'lidar'
+					},
 				{
 					'type': 'sensor.other.imu',
 					'x': 0.0, 'y': 0.0, 'z': 0.0,
@@ -148,17 +148,7 @@ class ContrastiveFC_Agent(autonomous_agent.AutonomousAgent):
 		speed = input_data['speed'][1]['speed']
 		compass = input_data['imu'][1][-1]
 		lidar = input_data['lidar'][1][:, :3]
-
-		result = {'rgb': rgb,
-                        'rgb_left': rgb_left,
-                        'rgb_right': rgb_right,
-                        'rgb_rear': rgb_rear,
-                        'lidar': lidar,
-                        'gps': gps,
-                        'speed': speed,
-                        'compass': compass,
-                                }
-		
+		result = {'rgb': rgb, 'rgb_left': rgb_left, 'rgb_right': rgb_right, 'rgb_rear': rgb_rear, 'lidar': lidar, 'gps': gps, 'speed': speed, 'compass': compass}
 		pos = self._get_position(result)
 		result['gps'] = pos
 		next_wp, next_cmd = self._route_planner.run_step(pos)
@@ -186,8 +176,9 @@ class ContrastiveFC_Agent(autonomous_agent.AutonomousAgent):
 		if self.step < self.config.seq_len:
 			rgb = torch.from_numpy(scale_and_crop_image(Image.fromarray(tick_data['rgb']), scale=self.config.scale, crop=self.config.input_resolution)).unsqueeze(0)
 			self.input_buffer['rgb'].append(rgb.to('cuda', dtype=torch.float32))
-			
-			if not self.config.ignore_sides: # ignore = True
+		
+            	
+			"""if not self.config.ignore_sides: # ignore = True
 				rgb_left = torch.from_numpy(scale_and_crop_image(Image.fromarray(tick_data['rgb_left']), scale=self.config.scale, crop=self.config.input_resolution)).unsqueeze(0)
 				self.input_buffer['rgb_left'].append(rgb_left.to('cuda', dtype=torch.float32))
 				
@@ -198,6 +189,8 @@ class ContrastiveFC_Agent(autonomous_agent.AutonomousAgent):
 				rgb_rear = torch.from_numpy(scale_and_crop_image(Image.fromarray(tick_data['rgb_rear']), scale=self.config.scale, crop=self.config.input_resolution)).unsqueeze(0)
 				self.input_buffer['rgb_rear'].append(rgb_rear.to('cuda', dtype=torch.float32))
 
+            """
+    
 			self.input_buffer['lidar'].append(tick_data['lidar'])
 			self.input_buffer['gps'].append(tick_data['gps'])
 			self.input_buffer['thetas'].append(tick_data['compass']) # TODO: look into thetas. I think I removed a couple theta lines
@@ -216,11 +209,11 @@ class ContrastiveFC_Agent(autonomous_agent.AutonomousAgent):
 											torch.FloatTensor([tick_data['target_point'][1]])]
 		target_point = torch.stack(tick_data['target_point'], dim=1).to('cuda', dtype=torch.float32)
 
-		encoding = []
+		#encoding = []
 		rgb = torch.from_numpy(scale_and_crop_image(Image.fromarray(tick_data['rgb']), scale=self.config.scale, crop=self.config.input_resolution)).unsqueeze(0)
 		self.input_buffer['rgb'].popleft()
 		self.input_buffer['rgb'].append(rgb.to('cuda', dtype=torch.float32))
-		encoding.append(self.net.image_encoder(list(self.input_buffer['rgb'])))
+		#encoding.append(self.net.image_encoder(list(self.input_buffer['rgb'])))
 	
 		"""
 		if not self.config.ignore_sides: # ignore = True
@@ -239,7 +232,7 @@ class ContrastiveFC_Agent(autonomous_agent.AutonomousAgent):
 			self.input_buffer['rgb_rear'].popleft()
 			self.input_buffer['rgb_rear'].append(rgb_rear.to('cuda', dtype=torch.float32))
 			encoding.append(self.net.image_encoder(list(self.input_buffer['rgb_rear'])))
-                """
+		"""
 
 		self.input_buffer['lidar'].popleft()
 		self.input_buffer['lidar'].append(tick_data['lidar'])
@@ -260,17 +253,20 @@ class ContrastiveFC_Agent(autonomous_agent.AutonomousAgent):
 					np.pi/2-curr_theta, -curr_x, -curr_y, np.pi/2-ego_theta, -ego_x, -ego_y)
 			lidar_transformed = torch.from_numpy(lidar_to_histogram_features(lidar_transformed, crop=self.config.input_resolution)).unsqueeze(0)
 			lidar_processed.append(lidar_transformed.to('cuda', dtype=torch.float32))
-		encoding.append(self.net.lidar_encoder(lidar_processed))
+		#encoding.append(self.net.lidar_encoder(lidar_processed))
 
 		#pred_wp = self.net(encoding, target_point)
 		#steer, throttle, brake, metadata = self.net.control_pid(pred_wp, gt_velocity)
 		#self.pid_metadata = metadata
 
 		# Get steering from network & throttle/brake from autopilot
-		steer = self.net_controls(self.input_data['rgb'], self.input_data['lidar'])
-		near_node, near_command = AutoPilot._waypoint_planner.run_step(gps) #TODO: haven't figured out where these are from
-		far_node, far_command = Autopilot._command_planner.run_step(gps) # TODO
-		_, throttle, brake, _ = AutoPilot._get_control_autopilot(near_node, far_node, tick_data)
+        #print("here")
+		steer = self.net_controls(rgb.to('cuda',dtype=torch.float32), lidar_transformed.to('cuda',dtype=torch.float32))
+		#near_node, near_command = Autopilot._waypoint_planner.run_step(gps) #TODO: haven't figured out where these are from
+		throttle=1 #far_node, far_command = Autopilot._command_planner.run_step(gps) # TODO
+		brake=0#_, throttle, brake, _ = AutoPilot._get_control_autopilot(near_node, far_node, tick_data)
+        #throttle = 1
+        #brake = 0
 
 		control = carla.VehicleControl()
 		control.steer = float(steer)
@@ -287,9 +283,9 @@ class ContrastiveFC_Agent(autonomous_agent.AutonomousAgent):
 
 		Image.fromarray(tick_data['rgb']).save(self.save_path / 'rgb' / ('%04d.png' % frame))
 		
-		outfile = open(self.save_path / 'meta' / ('%04d.json' % frame), 'w')
-		json.dump(self.pid_metadata, outfile, indent=4)
-		outfile.close()
+		#outfile = open(self.save_path / 'meta' / ('%04d.json' % frame), 'w')
+		#json.dump(self.pid_metadata, outfile, indent=4)
+		#outfile.close()
 
 	def destroy(self):
-		del self.net
+		del self.net_contrastive, self.net_controls
