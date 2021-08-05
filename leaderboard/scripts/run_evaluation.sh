7f403894113d77fa0c0668cca65e36c5dfa7d569
yewon-lee <yewonlee.lee@mail.utoronto.ca>
2021-08-04 20:22:10 -0400

    adding contrastiveFC integrated into autopilot

--- leaderboard/scripts/run_evaluation.sh
+++ leaderboard/scripts/run_evaluation.sh
@@ -11,16 +11,16 @@ export PYTHONPATH=$PYTHONPATH:scenario_runner
 
 export LEADERBOARD_ROOT=leaderboard
 export CHALLENGE_TRACK_CODENAME=SENSORS
-export PORT=2000 # same as the carla server port
-export TM_PORT=8000 # port for traffic manager, required when spawning multiple servers/clients
+export PORT=2002 # same as the carla server port
+export TM_PORT=8001 # port for traffic manager, required when spawning multiple servers/clients
 export DEBUG_CHALLENGE=0
 export REPETITIONS=1 # multiple evaluation runs
-export ROUTES=leaderboard/data/training_routes/routes_town07_tiny.xml
-export TEAM_AGENT=leaderboard/team_code/auto_pilot.py # agent
+export ROUTES=leaderboard/data/training_routes/routes_town04_short.xml
+export TEAM_AGENT=leaderboard/team_code/contrastiveFC_autopilot.py #late_fusion_agent.py # agent
 export TEAM_CONFIG=aim/log/aim_ckpt # model checkpoint, not required for expert
 export CHECKPOINT_ENDPOINT=results/sample_result.json # results file
 export SCENARIOS=leaderboard/data/scenarios/no_scenarios.json
-export SAVE_PATH=data/expert # path for saving episodes while evaluating
+export SAVE_PATH=data/test_sunny_town4_short # path for saving episodes while evaluating
 export RESUME=True
 
 python3 ${LEADERBOARD_ROOT}/leaderboard/leaderboard_evaluator.py \
